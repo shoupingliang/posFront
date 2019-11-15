c29e984bd23650d06a36a3bae4ad3e68c2bdb351
shoupingliang <shoupingliang@gmail.com>
2019-11-15 15:00:01 +0100

    finish drink list

--- src/app/drink/drink.component.ts
+++ src/app/drink/drink.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { DrinkService } from '../shared/drink.service';
-import { }
+import { Drink } from '../shared/drink.model'
 
 @Component({
   selector: 'app-drink',
@@ -14,11 +14,12 @@ export class DrinkComponent implements OnInit {
   constructor(private drinkService: DrinkService) { }
 
   ngOnInit() {
+    this.refreshDrinkList();
   }
 
   refreshDrinkList(){
     this.drinkService.getDrinkList().subscribe((res)=>{
-      this.drinkService.drinks = res;
+      this.drinkService.drinks = res as Drink[];
     })
   }
 
